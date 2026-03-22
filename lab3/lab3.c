e443c475294059d75308e2c1098ed7d6bad04247
MaRZ113 <grumpycat.mr@gmail.com>
2026-03-22 17:57:11 +1000

    added 3rd lab

---
+++ lab3/lab3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+char str[85]; 
+    int i;
+    printf("your string:\n");
+    fgets(str, sizeof(str), stdin);
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == 'a') {
+            str[i] = 'A';
+        } else if (str[i] == 'b') {
+            str[i] = 'B';
+        }
+    }
+    printf("your fixed string:\n%s", str);
+    return 0;
+}
