74d946526cd499fe6f89c982742f6c9cea9fb5cb
MaRZ113 <grumpycat.mr@gmail.com>
2026-03-23 13:58:11 +1000

    added C source file for 4th lab

---
+++ lab4/triangle.c
@@ -0,0 +1,11 @@
+#include <math.h>
+#include "triangle.h"
+
+double get_perimeter(double a, double b, double c) {
+    return a + b + c;
+}
+
+double get_area(double a, double b, double c) {
+    double p = get_perimeter(a, b, c) / 2.0;
+    return sqrt(p * (p - a) * (p - b) * (p - c));
+}
